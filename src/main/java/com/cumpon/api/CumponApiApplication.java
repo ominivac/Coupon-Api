8c005690701fa1bbca70baaaa49111c8a0058e0d
ominivac <robertokbs@gmail.com>
2026-05-31 17:26:54 -0300

    criação do projeto

---
+++ src/main/java/com/cumpon/api/CumponApiApplication.java
@@ -0,0 +1,13 @@
+package com.cumpon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumponApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumponApiApplication.class, args);
+	}
+
+}
